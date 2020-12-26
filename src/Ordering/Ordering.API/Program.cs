606b9c9d8be0be7e7164b8f805f84587d0ae933e
kannansgithub <kannan-inbox@outlook.com>
2020-12-26 12:13:17 +0530

    Added Ordering Service

---
+++ src/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Data;
+namespace Ordering.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host=CreateHostBuilder(args).Build();
+            CreateAndSeedDatabase(host);
+            host.Run();
+        }
+private static void CreateAndSeedDatabase(IHost host){
+    using(var scope=host.Services.CreateScope()){
+        
+    }
+}
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
